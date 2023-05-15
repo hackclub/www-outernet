45577d9d12bdc7d1e12a6d737371656591e9adc4
Sam Poder <39828164+sampoder@users.noreply.github.com>
2023-05-15 17:17:38 +0800

    Add Meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,79 @@
 import "@/styles/globals.scss";
+import Head from "next/head";
+
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return(
+        <>
+        <Meta
+          name="Outernet ~ July 28th to 31st 2023 in Vermont" // site name
+          title="Outernet ~ July 28th to 31st 2023 in Vermont" // page title
+          description="This summer, join us in building a space where you can indulge in pure creativity. No preconceived notions. No expectations. Just a space for you to create whatever you want." // page description
+          image="https://user-images.githubusercontent.com/39828164/236823165-1b5c32e8-4143-4b36-b5ff-a76be2f1ac65.jpeg" // large summary card image URL
+          color="#ec3750" // theme color
+         />
+        <Component {...pageProps} />
+    </>);
 }
+
+
+const makeTitle = (title, name) =>
+  title === name ? title : `${title} – ${name}`
+
+const Meta = ({
+  name = 'Hack Club', // site name
+  title = 'Hack Club', // page title
+  description,
+  image,
+  color = '#ec3750',
+  manifest = 'https://assets.hackclub.com/favicons/site.webmanifest',
+  children
+}) => (
+  <Head>
+    <meta key="og_locale" property="og:locale" content="en_US" />
+    <meta key="og_type" property="og:type" content="website" />
+    <meta key="og_site" property="og:site_name" content={name} />
+    <meta key="tw_site" name="twitter:site" content="@hackclub" />
+    <title key="title">{makeTitle(title, name)}</title>
+    <meta key="og_title" property="og:title" content={makeTitle(title, name)} />
+    <meta
+      key="tw_title"
+      name="twitter:title"
+      content={makeTitle(title, name)}
+    />
+    {description && (
+      <>
+        <meta key="desc" name="description" content={description} />
+        <meta key="og_desc" property="og:description" content={description} />
+        <meta key="tw_desc" name="twitter:description" content={description} />
+      </>
+    )}
+    {image && (
+      <>
+        <meta key="og_img" property="og:image" content={image} />
+        <meta key="tw_card" name="twitter:card" content="summary_large_image" />
+        <meta key="tw_img" name="twitter:image" content={image} />
+      </>
+    )}
+    <meta key="theme_color" name="theme-color" content={color} />
+    <meta key="tile_color" name="msapplication-TileColor" content={color} />
+    <link
+      rel="shortcut icon"
+      type="image/png"
+      href="/favicon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      href="/favicon.png"
+    />
+    <link
+      rel="favicon"
+      type="image/png"
+      href="/favicon.png"
+    />
+    {manifest && <link key="manifest" rel="manifest" href={manifest} />}
+    {children}
+  </Head>
+)
+
