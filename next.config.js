978e2b30dd079b7cf416a39a028df0bf32a9013e
Sam Poder <sam.r.poder@gmail.com>
2023-05-23 16:39:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/stipends',
+        destination: 'https://airtable.com/shr4Hx3nkLvOt7GMQ',
+        permanent: true,
+      },
+      {
+        source: '/shuttles',
+        destination: 'https://airtable.com/shrKxW4Vga4uWFAL7',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
