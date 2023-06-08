cf31c8a48c85cef67e7de5f066e83ecb541fef60
Sam Poder <sam.r.poder@gmail.com>
2023-06-08 22:53:06 +0200

    Update Travel.js

--- components/Travel.js
+++ components/Travel.js
@@ -18,7 +18,7 @@ export default function Travel() {
           <img src="https://lh3.googleusercontent.com/wTQaY8GnZQbkoSvsyDgb-GK27Pc4nIBnoGk2lJWMWdPQf1RPrCwX5L6GOwv5bC7SFMSsCkk4mnK2gNIW4tSpriMNUHDCnsdNjoBeVEqHXTWUiiEWfytP0Q=s1600" />
           <p>
             Outernet is taking place in Vermont's Northeast Kingdom (Cabot,
-            05647) at <a href="https://www.covenanthillsvt.org/who-we-are.html">Covenant Hills Campground</a>. Shuttles will be provided from:
+            05647) at <a href="https://www.covenanthillsvt.org/who-we-are.html" target="_blank">Covenant Hills Campground</a>. Shuttles will be provided from:
           </p>
 
           <ul>
@@ -29,7 +29,7 @@ export default function Travel() {
           </ul>
           <p>
             The venue is also accessible via car{" "}
-            <a href="https://goo.gl/maps/zhMntt1f3KT6TZae9">(directions)</a>.
+            <a href="https://goo.gl/maps/zhMntt1f3KT6TZae9" target="_blank">(directions)</a>.
           </p>
           <p>
             To ensure everyone has a chance to make it, our donors have funded a
@@ -40,7 +40,7 @@ export default function Travel() {
             On your registation form, indicate that you'd like to recieve
             details about long-distance travel and we'll email the information
             about stipends and discounted flights to you and your parents. View our{" "}
-            <a href="https://hack.af/outernet-travel-guide">travel guide</a>{" "}
+            <a href="https://hack.af/outernet-travel-guide" target="_blank">travel guide</a>{" "}
             for additional guidance.
           </p>
         </Fade>
