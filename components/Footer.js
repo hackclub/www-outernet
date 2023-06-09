2f86b302829945f3a9a9bb8a762a5757cb3ab7ac
Sam Poder <sam.r.poder@gmail.com>
2023-06-09 16:04:29 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <>
             <div className={styles.footer}>
                 <img className={styles.footerImage} src="./assets/mountains-two.svg"/>
-                <h3 className={styles.footerOne}>An Out-Of-Doors, Make-It-Yours Adventure<br />From Us To You</h3>
+                <h3 className={styles.footerOne}>An Out-Of-Doors, Make-It-Yours Adventure. From Us To You.</h3>
             </div>
         </>
     )
