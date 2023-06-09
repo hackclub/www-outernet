491271f7c74c47708476f81fb3041d382235baeb
Sam Poder <sam.r.poder@gmail.com>
2023-06-09 16:02:33 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,12 +6,6 @@ export default function Footer() {
             <div className={styles.footer}>
                 <img className={styles.footerImage} src="./assets/mountains-two.svg"/>
                 <h3 className={styles.footerOne}>An Out-Of-Doors, Make-It-Yours Adventure<br />From Us To You</h3>
-                <h3 className={styles.footerTwo}><br />
-                    <a target="_blank" href="https://hackclub.org">Hack Club</a> | 
-                    <a target="_blank" href="https://instagram.com/starthackclub/"> Instagram</a> | 
-                    <a target="_blank" href="https://github.com/hackclub/www-outernet"> GitHub</a> | 
-                    <a target="_blank" href=""> Bank</a>
-                </h3>
             </div>
         </>
     )
