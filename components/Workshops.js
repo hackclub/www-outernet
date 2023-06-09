d044a1f3f087645dd7a332116683221214276a71
Belle See <belle@Belles-Laptop.hsd1.vt.comcast.net>
2023-06-09 10:06:24 -0400

    balls

--- components/Workshops.js
+++ components/Workshops.js
@@ -5,127 +5,127 @@ import Matter from "matter-js";
 import { Fade, Zoom } from "react-reveal";
 
 export default function Workshops() {
-    const balls = [
-        {
-            heading: "The Cookhouse",
-            text: "Did someone say dinner?."
-        },
-        {
-            heading: "The Lab",
-            text: "Come and learn something new!"
-        },
-        {
-            heading: "The Hardware Shack",
-            text: "Find makerware to hack on"
-        },
-        {
-            heading: "Orpheus Café & Bakery",
-            text: "We're serving matcha, coffee, tea, and baked goods!"
-        },
-        {
-            heading: "Passport Authority",
-            text: "We issue documents to explore the Outernet!"
-        },
-        {
-            heading: "OuterLAN",
-            text: "We're building a site-wide intranet!"
-        }
-    ];
-    const ballsRef = useRef([]);
-    const containerRef = useRef();
-    const requestRef = useRef();
-    const engineRef = useRef();
+    // const balls = [
+    //     {
+    //         heading: "The Cookhouse",
+    //         text: "Did someone say dinner?."
+    //     },
+    //     {
+    //         heading: "The Lab",
+    //         text: "Come and learn something new!"
+    //     },
+    //     {
+    //         heading: "The Hardware Shack",
+    //         text: "Find makerware to hack on"
+    //     },
+    //     {
+    //         heading: "Orpheus Café & Bakery",
+    //         text: "We're serving matcha, coffee, tea, and baked goods!"
+    //     },
+    //     {
+    //         heading: "Passport Authority",
+    //         text: "We issue documents to explore the Outernet!"
+    //     },
+    //     {
+    //         heading: "OuterLAN",
+    //         text: "We're building a site-wide intranet!"
+    //     }
+    // ];
+    // const ballsRef = useRef([]);
+    // const containerRef = useRef();
+    // const requestRef = useRef();
+    // const engineRef = useRef();
 
-    useEffect(() => {
-        engineRef.current = Matter.Engine.create();
-        const engine = engineRef.current;
+    // useEffect(() => {
+    //     engineRef.current = Matter.Engine.create();
+    //     const engine = engineRef.current;
 
-        let boxes = [];
-        let x = 110,
-            y = 110;
-        for (let ball of ballsRef.current) {
-            boxes.push({
-                body: Matter.Bodies.circle(x, y, 230 / 2),
-                elem: ball,
-                render() {
-                    const { x, y } = this.body.position;
-                    this.elem.style.top = `${y - 230 / 2}px`;
-                    this.elem.style.left = `${x - 230 / 2}px`;
-                    this.elem.style.transform = `rotate(${this.body.angle}rad)`;
-                }
-            });
-            x += 230;
-            if (x >= containerRef.current.clientWidth) {
-                x = 230;
-                y += 230;
-            }
-        }
+    //     let boxes = [];
+    //     let x = 110,
+    //         y = 110;
+    //     for (let ball of ballsRef.current) {
+    //         boxes.push({
+    //             body: Matter.Bodies.circle(x, y, 230 / 2),
+    //             elem: ball,
+    //             render() {
+    //                 const { x, y } = this.body.position;
+    //                 this.elem.style.top = `${y - 230 / 2}px`;
+    //                 this.elem.style.left = `${x - 230 / 2}px`;
+    //                 this.elem.style.transform = `rotate(${this.body.angle}rad)`;
+    //             }
+    //         });
+    //         x += 230;
+    //         if (x >= containerRef.current.clientWidth) {
+    //             x = 230;
+    //             y += 230;
+    //         }
+    //     }
 
-        const { top, right, bottom, left } =
-            containerRef.current.getBoundingClientRect();
+    //     const { top, right, bottom, left } =
+    //         containerRef.current.getBoundingClientRect();
 
-        Matter.Composite.add(engine.world, [
-            ...boxes.map(box => box.body),
-            Matter.Bodies.rectangle(
-                0,
-                containerRef.current.clientHeight - 1,
-                containerRef.current.clientWidth * 2,
-                1,
-                { isStatic: true }
-            ),
-            Matter.Bodies.rectangle(
-                0,
-                0,
-                1,
-                containerRef.current.clientHeight * 2,
-                { isStatic: true }
-            ),
-            Matter.Bodies.rectangle(
-                containerRef.current.clientWidth - 1,
-                0,
-                1,
-                containerRef.current.clientHeight * 2,
-                { isStatic: true }
-            ),
-            Matter.Bodies.rectangle(
-                0,
-                0,
-                containerRef.current.clientWidth * 2,
-                1,
-                { isStatic: true }
-            ),
-            // Matter.MouseConstraint.create(engine, {
-            //     element: containerRef.current,
-            //     constraint: {
-            //         render: {
-            //           visible: true
-            //         },
-            //         stiffness: 0.8
-            //       }
-            // }),
-        ]);
+    //     Matter.Composite.add(engine.world, [
+    //         ...boxes.map(box => box.body),
+    //         Matter.Bodies.rectangle(
+    //             0,
+    //             containerRef.current.clientHeight - 1,
+    //             containerRef.current.clientWidth * 2,
+    //             1,
+    //             { isStatic: true }
+    //         ),
+    //         Matter.Bodies.rectangle(
+    //             0,
+    //             0,
+    //             1,
+    //             containerRef.current.clientHeight * 2,
+    //             { isStatic: true }
+    //         ),
+    //         Matter.Bodies.rectangle(
+    //             containerRef.current.clientWidth - 1,
+    //             0,
+    //             1,
+    //             containerRef.current.clientHeight * 2,
+    //             { isStatic: true }
+    //         ),
+    //         Matter.Bodies.rectangle(
+    //             0,
+    //             0,
+    //             containerRef.current.clientWidth * 2,
+    //             1,
+    //             { isStatic: true }
+    //         ),
+    //         // Matter.MouseConstraint.create(engine, {
+    //         //     element: containerRef.current,
+    //         //     constraint: {
+    //         //         render: {
+    //         //           visible: true
+    //         //         },
+    //         //         stiffness: 0.8
+    //         //       }
+    //         // }),
+    //     ]);
  
-        (function rerender() {
-            // Rerender every box
-            for (let box of boxes) box.render();
-            Matter.Engine.update(engine);
-            requestRef.current = requestAnimationFrame(rerender);
-        })();
+    //     (function rerender() {
+    //         // Rerender every box
+    //         for (let box of boxes) box.render();
+    //         Matter.Engine.update(engine);
+    //         requestRef.current = requestAnimationFrame(rerender);
+    //     })();
 
-        return () => {
-            cancelAnimationFrame(requestRef.current);
-            Matter.Engine.clear(engineRef.current);
-        };
-    }, []);
+    //     return () => {
+    //         cancelAnimationFrame(requestRef.current);
+    //         Matter.Engine.clear(engineRef.current);
+    //     };
+    // }, []);
 
 
-    useEffect(()=> {
-        if (typeof window != undefined) {
-            let ball = document.getElementById('ball')
-            ball.removeEventListener("mousewheel", function() {return false})
-            ball.removeEventListener("DOMMouseScroll", function() {return false})
-        }
-    }, [])
+    // useEffect(()=> {
+    //     if (typeof window != undefined) {
+    //         let ball = document.getElementById('ball')
+    //         ball.removeEventListener("mousewheel", function() {return false})
+    //         ball.removeEventListener("DOMMouseScroll", function() {return false})
+    //     }
+    // }, [])
 
     return (
         <div className={styles.wrapper}>
@@ -158,7 +158,7 @@ export default function Workshops() {
                     </a>
                 </p>
             </div>
-            <div
+            {/* <div
                 style={{
                     position: "relative",
                     cursor: "default !important",
@@ -178,7 +178,7 @@ export default function Workshops() {
                         </div>
                     </div>
                 ))}
-            </div>
+            </div> */}
         </div>
     );
 }
